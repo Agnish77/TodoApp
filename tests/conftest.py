115077b88392ae486be7b030912aee90d04ee387
Agnish Paul <97803357+Agnish77@users.noreply.github.com>
2026-01-26 19:46:53 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,20 @@
 import pytest
-from app import app
-from data import db
-from model import User
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt()
+from app import app, db
+from app.models import User
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config["JWT_SECRET_KEY"] = "test-jwt-secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 
-    with app.app_context():
-        db.create_all()
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
 
-        # create test user
-        password = bcrypt.generate_password_hash("testpass").decode("utf-8")
-        user = User(username="testuser", password=password)
-        db.session.add(user)
-        db.session.commit()
+            user = User(username="testuser")
+            user.set_password("testpass")
+            db.session.add(user)
+            db.session.commit()
 
-        yield app.test_client()
-
-        db.session.remove()
-        db.drop_all()
+        yield client
