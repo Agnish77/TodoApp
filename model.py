11bfd7e6d6c9682607c386f24b49f5d2855eca4c
Agnish Paul <97803357+Agnish77@users.noreply.github.com>
2026-01-26 19:57:46 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,20 @@
 from data import db
 from datetime import datetime
 from flask_login import UserMixin
-class User(db.Model,UserMixin):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(150),unique=True,nullable=False)
-    password=db.Column(db.String(200),nullable=False)
-    created_at=db.Column(db.DateTime,default=datetime.utcnow)
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
 
 
 class Todo(db.Model):
